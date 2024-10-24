78a08f5e5cf83c294a28fe1db8b4cbc40ca7a1bd
yenugukeerthana <yenugukeerthana@gmail.com>
2024-10-24 22:44:41 +0100

    Add Click Me Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [onButtonClick, setOnButtonClick]=useState<Boolean>(false)
+  const onButtonClickEvent=()=>{
+    setOnButtonClick(true)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <button className='button' onClick={onButtonClickEvent}>
+          Click Me
+        </button>
+        {onButtonClick && <h1>You clicked me</h1>}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
